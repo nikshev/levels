aaecf51d39922f1c09ac9055e49d7f77a751d3e9
eugene <nikshev81@gmail.com>
2016-11-16 07:46:14 +0200

    ANother commit

--- levels.R
+++ levels.R
@@ -1,2 +1,10 @@
 #Get data from file
-data<-read.table("data.csv")
+library(cluster)
+library(fpc)
+data<-na.omit(read.csv("data.csv"))
+dat<-data[,3:14]
+clus<-kmeans(dat,25,nstart = 20)
+plotcluster(dat, clus$cluster)
+clusplot(dat, clus$cluster, color=TRUE, shade=TRUE, 
+         labels=2, lines=0)
+clus
